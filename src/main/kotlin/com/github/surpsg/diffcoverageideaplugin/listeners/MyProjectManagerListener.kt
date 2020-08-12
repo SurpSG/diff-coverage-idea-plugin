3e0e9f5ee2978f6059a54277dea2215873c4aea7
Sergii Gnatiuk <sergii.gnatiuk@worldapp.com>
2020-08-13 00:28:16 +0300

    change platform version

--- src/main/kotlin/com/github/surpsg/diffcoverageideaplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/surpsg/diffcoverageideaplugin/listeners/MyProjectManagerListener.kt
@@ -1,12 +1,14 @@
 package com.github.surpsg.diffcoverageideaplugin.listeners
 
+import com.github.surpsg.diffcoverageideaplugin.services.MyProjectService
+import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import com.github.surpsg.diffcoverageideaplugin.services.MyProjectService
+import com.intellij.openapi.ui.Messages
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
     override fun projectOpened(project: Project) {
-        project.getService(MyProjectService::class.java)
+        Messages.showMessageDialog(project, "yo helo", "I'm title", Messages.getInformationIcon());
     }
 }
