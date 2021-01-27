ad2f9fcf5e2433018a39b18410337971797fa075
Sergii Gnatiuk <sergii.gnatiuk@worldapp.com>
2021-01-28 01:24:32 +0200

    implemented run of diff coverage task by shortcut

--- src/main/kotlin/com/github/surpsg/diffcoverage/listeners/GradleStartup.kt
+++ src/main/kotlin/com/github/surpsg/diffcoverage/listeners/GradleStartup.kt
@@ -1,49 +1,34 @@
 package com.github.surpsg.diffcoverage.listeners
 
+import com.intellij.openapi.externalSystem.action.ExternalSystemActionUtil
+import com.intellij.openapi.externalSystem.model.ProjectSystemId
+import com.intellij.openapi.externalSystem.model.task.TaskData
+import com.intellij.openapi.externalSystem.service.execution.ProgressExecutionMode
 import com.intellij.openapi.externalSystem.service.project.manage.ExternalProjectsManager
+import com.intellij.openapi.externalSystem.task.TaskCallback
+import com.intellij.openapi.externalSystem.util.ExternalSystemUtil
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import com.intellij.openapi.ui.Messages
 import org.jetbrains.plugins.gradle.settings.GradleExtensionsSettings
 import org.jetbrains.plugins.gradle.settings.GradleSettings
+import org.jetbrains.plugins.gradle.util.GradleUtil
 
 internal class GradleStartup : StartupActivity.DumbAware {
     override fun runActivity(project: Project) {
-        println("Is gradle project ${GradleSettings.getInstance(project).linkedProjectsSettings.isNotEmpty()}")
-
-        ExternalProjectsManager.getInstance(project).runWhenInitialized {
-            val diffCoveragePluginAppliedTo = lookupDiffCoveragePlugin(project)
-            val (icon, message) = if (diffCoveragePluginAppliedTo.isNotEmpty()) {
-                Messages.getInformationIcon() to diffCoveragePluginAppliedTo.asSequence().joinToString("\n") {
-                    "DiffCoverage is applied to: \n ${it.key}\n\t${it.value.joinToString("\n\t")}"
-                }
-            } else {
-                Messages.getWarningIcon() to "DiffCoverage is not applied. Visit https://github.com/form-com/diff-coverage-gradle"
-            }
-
+        val linkedProjectsSettings = GradleSettings.getInstance(project).linkedProjectsSettings
+        if (linkedProjectsSettings.isEmpty()) {
             Messages.showMessageDialog(
                     project,
-                    message,
+                    "Not a Gradle project",
                     "DiffCoverage plugin",
-                    icon
-            );
+                    Messages.getWarningIcon()
+            )
+            return
         }
     }
 
-    private fun lookupDiffCoveragePlugin(project: Project): Map<String, Set<String>> {
-        val projectsWithDiffCoverPlugin = mutableMapOf<String, MutableSet<String>>()
-        for (projectEntry in GradleExtensionsSettings.getInstance(project).projects) {
-            for (moduleExtension in projectEntry.value.extensions) {
-                if (moduleExtension.value.tasksMap.containsKey("diffCoverage")) {
-                    projectsWithDiffCoverPlugin.computeIfAbsent(projectEntry.key) {
-                        mutableSetOf()
-                    }.add(moduleExtension.key)
-                }
-            }
-        }
 
-        println(projectsWithDiffCoverPlugin)
-
-        return projectsWithDiffCoverPlugin
-    }
 }
