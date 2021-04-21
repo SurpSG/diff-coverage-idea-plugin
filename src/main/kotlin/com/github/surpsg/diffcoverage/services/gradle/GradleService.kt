8c0abc8cd95d645df4f778dd82a0f879c8ded6ad
Sergii Gnatiuk <sergii.gnatiuk@worldapp.com>
2021-04-21 22:14:34 +0300

    Show Diff Coverage report notification if diffCoverage Gradle task failed

--- src/main/kotlin/com/github/surpsg/diffcoverage/services/gradle/GradleService.kt
+++ src/main/kotlin/com/github/surpsg/diffcoverage/services/gradle/GradleService.kt
@@ -1,5 +1,6 @@
 package com.github.surpsg.diffcoverage.services.gradle
 
+import com.github.surpsg.diffcoverage.coroutine.BACKGROUND_SCOPE
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.externalSystem.action.ExternalSystemActionUtil
 import com.intellij.openapi.externalSystem.model.ProjectSystemId
@@ -8,7 +9,10 @@ import com.intellij.openapi.externalSystem.service.execution.ProgressExecutionMo
 import com.intellij.openapi.externalSystem.task.TaskCallback
 import com.intellij.openapi.externalSystem.util.ExternalSystemUtil
 import com.intellij.openapi.project.Project
+import kotlinx.coroutines.future.asDeferred
+import kotlinx.coroutines.withContext
 import org.jetbrains.plugins.gradle.settings.GradleSettings
+import java.util.concurrent.CompletableFuture
 
 @Service
 class GradleService(private val project: Project) {
@@ -17,10 +21,27 @@ class GradleService(private val project: Project) {
         return !GradleSettings.getInstance(project).linkedProjectsSettings.isEmpty()
     }
 
-    fun executeGradleTask(
+    suspend fun suspendableExecuteGradleTask(
+        taskName: String,
+        modulePath: String
+    ): Boolean = withContext(BACKGROUND_SCOPE.coroutineContext) {
+        val result = CompletableFuture<Boolean>()
+        executeGradleTask(taskName, modulePath, object : TaskCallback {
+            override fun onSuccess() {
+                result.complete(true)
+            }
+
+            override fun onFailure() {
+                result.complete(false)
+            }
+        })
+        result.asDeferred().await()
+    }
+
+    private fun executeGradleTask(
         taskName: String,
         modulePath: String,
-        successCallback: () -> Unit
+        taskCallback: TaskCallback
     ) {
         val projectSystemId = ProjectSystemId("GRADLE")
         val taskExecutionInfo = ExternalSystemActionUtil.buildTaskInfo(
@@ -36,15 +57,7 @@ class GradleService(private val project: Project) {
             taskExecutionInfo.executorId,
             project,
             projectSystemId,
-            object : TaskCallback {
-                override fun onSuccess() {
-                    successCallback()
-                }
-
-                override fun onFailure() {
-                    println("task $taskName failed")
-                }
-            },
+            taskCallback,
             ProgressExecutionMode.IN_BACKGROUND_ASYNC,
             false
         )
